e551d2b569879d336e5a2061432cb8e8fdd683b2
mariosilvadistribution <mariosilvadistribution@gmail.com>
2025-07-25 11:46:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.seccion').forEach(sec => {
+  observer.observe(sec);
+});
